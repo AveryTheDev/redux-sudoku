25a5260f860ae8ebd78524ada5aa13fa6ffb201e
abrown945 <abrown945@gmail.com>
2019-12-17 17:57:22 -0500

    general layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Header from "./scenes/Header";
+import Puzzle from "./scenes/Puzzle";
+import Settings from "./scenes/Settings";
+import "./styles/global.scss";
 
 function App() {
-  return <div className="App">hey</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="App_row">
+        <Puzzle />
+        <Settings />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/scenes/header.js
@@ -0,0 +1,12 @@
+import "../styles/header.scss";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="header_title"> Sudoku </h1>
+    </div>
+  );
+};
+
+export default Header;
